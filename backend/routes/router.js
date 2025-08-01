b4a57b299ec27ab13f46b60b2918432b11ff7855
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-08-01 22:58:14 +0530

    tracking

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 const router = Router();
+import vehicleVerification from "../controllers/vehicle.verification.js";
 
 import { registerUser, loginUser, addToContact, addNickName, checkForRequest, acceptRequest, getMyContacts } from "../controllers/auth.controller.js";
 
@@ -10,7 +11,8 @@ router.post("/addcontact", addToContact);
 router.put("/addnickname", addNickName);
 router.get("/checkrequest", checkForRequest);
 router.put("/acceptrequest", acceptRequest);
-router.get("/mycontacts" , getMyContacts);
+router.get("/mycontacts", getMyContacts);
+router.post("/verifyvehicle", vehicleVerification);
 
 
 
